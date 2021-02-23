a156053d5cc430f6fc62ef46e50b44700af55c31
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-02-24 00:44:59 +0300

    add new site

---
+++ psyc/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Tabs
+  var tabs = document.querySelectorAll('.tab');
+  initTabs(tabs);
+  var mobileButtonOpen = document.querySelector('#openMenu');
+  var mobileButtonClose = document.querySelector('#closeMenu');
+  var menu = document.querySelector('#mobileNav');
+  mobileButtonOpen.addEventListener('click', function () {
+    menu.classList.add('active');
+  });
+  mobileButtonClose.addEventListener('click', function () {
+    menu.classList.remove('active');
+  });
+});
