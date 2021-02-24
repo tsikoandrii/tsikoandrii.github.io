332222112ea8af0ce23218d99fc5d18d2e63f355
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-02-24 06:22:00 +0300

    add

--- psyc/js/app.js
+++ psyc/js/app.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   // Tabs
   var tabs = document.querySelectorAll('.tab');
   initTabs(tabs);
   var mobileButtonOpen = document.querySelector('#openMenu');
   var mobileButtonClose = document.querySelector('#closeMenu');
   var menu = document.querySelector('#mobileNav');
-  mobileButtonOpen.addEventListener('click', function () {
+  mobileButtonOpen.addEventListener('click', function() {
     menu.classList.add('active');
   });
-  mobileButtonClose.addEventListener('click', function () {
+  mobileButtonClose.addEventListener('click', function() {
     menu.classList.remove('active');
   });
 });
